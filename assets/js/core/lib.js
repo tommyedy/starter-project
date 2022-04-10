36e9d73750f0c11602ae0ee42412e99619d84487
Tommy <tommy@grip-principle.com>
2022-04-10 11:29:24 +0700

    change to hmvc structure folder

--- assets/js/core/lib.js
+++
@@ -1,7 +0,0 @@
-function show_notification_error(messages = null) {
-    return toastr.error(messages);
-}
-
-function show_notification_success(messages= null) {
-    return toastr.error(messages);
-}
